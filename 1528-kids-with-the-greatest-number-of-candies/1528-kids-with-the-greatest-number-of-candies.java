92990ce1cdac1fdadce5c5367a4f908c8702cde7
Gulshan Sharma <91317889+Gulshan-Gits@users.noreply.github.com>
2024-05-23 12:13:37 +0530

    Time: 0 ms (100%), Space: 42.2 MB (48.45%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> result = new ArrayList<Boolean>(candies.length);
+        int max = candies[0];
+        for(int i=1; i<candies.length; i++){
+            if(max<candies[i]){
+                max = candies[i];
+            }
+        }
+        for(int i=0; i<candies.length; i++){
+            if((candies[i]+extraCandies)>=max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
