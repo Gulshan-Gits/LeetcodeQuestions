6dd618e84ac1cd0f4123bf60513d9f57177dd131
Gulshan Sharma <91317889+Gulshan-Gits@users.noreply.github.com>
2025-05-19 22:26:33 +0530

    Time: 2 ms (75.03%), Space: 53.4 MB (30.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,18 @@
-public class Solution {
+class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int count = 0;
+        int res = 0;
+        for(int i=0; i<nums.length; i++){
+            if(count==0){
+                res = nums[i];
+            }
+            
+            if (res == nums[i]){
+                count = count + 1;
+            } else {
+                count = count - 1;
+            }
+        }
+        return res;
     }
 }
